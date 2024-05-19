2321fed109252d6721f4239e124e002ca0f11daa
mitsuYashi <hm20011027@gmail.com>
2024-05-20 01:04:14 +0900

    feat: loading背景

--- src/app/pdf/page.tsx
+++ src/app/pdf/page.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 
 export default function Home() {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<div style={{height: "100vh", width: "100vw", backgroundColor: "white"}}>Loading...</div>}>
             <RenderPDF />
         </Suspense>
     );
